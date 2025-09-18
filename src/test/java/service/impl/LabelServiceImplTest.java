9260cab6f2f4b071925c0632ae0745513bb87a8d
Dinar763 <gizzyatovdinar@gmail.com>
2025-09-19 00:26:38 +0400

    #3

--- src/test/java/service/impl/LabelServiceImplTest.java
+++ src/test/java/service/impl/LabelServiceImplTest.java
@@ -24,6 +24,7 @@ class LabelServiceImplTest {
 
     private LabelService labelService;
 
+
     private final Label testLabel1 = new Label(1L, "Label Test1", new ArrayList<>());
     private final Label testLabel2 = new Label(2L, "Label Test1", new ArrayList<>());
     private final List<Label> labels = List.of(testLabel1, testLabel2);
